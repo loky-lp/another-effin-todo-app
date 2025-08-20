5f722b2de9ebe15c530d779939a7d23499a876f9
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2025-08-20 13:47:53 +0200

    feat: add `/api/auth/login` route

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,38 @@
+'use server'
+
+import { json } from '@/lib/api'
+import { createJwt } from '@/lib/auth'
+import { prisma } from '@/lib/prisma'
+import { loginFormSchema } from '@/lib/validators/auth'
+import { verify } from '@node-rs/argon2'
+
+export async function POST(request: Request): Promise<Response> {
+	if (!request.headers.get('Content-Type')?.startsWith('multipart/form-data'))
+		return json({ name: 'BAD_REQUEST' }, { status: 400 })
+
+	const formData = await request.formData()
+
+	const u = loginFormSchema.safeParse(Object.fromEntries(formData.entries()))
+
+	if (!u.success)
+		return json({ name: 'BAD_REQUEST' }, { status: 400 })
+
+	const user = await prisma.user.findUnique({
+		where: {
+			email: u.data.email,
+		}
+	})
+
+	if (!user || !await verify(user.password, u.data.password))
+		return json({ name: 'BAD_REQUEST' }, { status: 400 })
+
+	const jwt = await createJwt({
+		sub: user.id,
+		email: user.email,
+		name: user.name
+	})
+
+	return json({
+		token: jwt,
+	})
+}
